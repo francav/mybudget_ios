7c1bca3160f1cd167fce3388b5a76304ca49b4c4
Victor Franca <victorfranca@macbook-infox-wifi.infox.intranet>
2018-08-24 07:56:40 -0300

    adicionando biblioteca iCarousel

--- em3anos/home/lancamentos/LancamentoViewController.swift
+++ em3anos/home/lancamentos/LancamentoViewController.swift
@@ -3,49 +3,57 @@
 //  em3anos
 //
 //  Created by Victor Franca on 23/08/18.
-//  Copyright © 2018 Victor Franca. All rights reserved.
+//  Copyright © 18 Victor Franca. All rights reserved.
 //
 
 import UIKit
 
-class LancamentoViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
-
-    @IBOutlet weak var periodoPicker: UIPickerView!
+class LancamentoViewController: UIViewController, iCarouselDataSource, iCarouselDelegate
+{
+    @IBOutlet weak var periodoCarousel: iCarousel!
+    @IBOutlet weak var lblPeriodoEscolhido: UILabel!
     
     var periodos: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        periodos = ["08/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018", "09/2018"]
+        periodos = ["08/18", "09/18", "10/18", "11/18", "12/18", "01/19", "02/19", "03/19", "04/19", "05/19", "06/19", "07/19", "08/19", "09/19", "10/19", "11/19", "12/19", "01/20", "02/20", "03/20", "04/20", "05/20", "06/20", "07/20", "08/20", "09/20", "10/20", "11/20", "12/20", "01/21", "02/21", "03/21", "04/21", "05/21", "06/21", "07/21"]
         
-        periodoPicker.delegate = self
-
+        periodoCarousel.type = .coverFlow2
+        
+        periodoCarousel.reloadData()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return periodos.count
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        let tempView = UIView(frame: CGRect(x: 0, y: 0, width: 250, height: 48))
+        
+        var label: UILabel
+        label = UILabel(frame: tempView.bounds)
+        label.backgroundColor = .clear
+        label.textAlignment = .center
+        label.font = label.font.withSize(25)
+        label.tag = 1
+        print(periodos[index])
+        label.text = periodos[index]
+        tempView.addSubview(label)
+        
+        return tempView
+    }
+    
+    func carouselDidEndScrollingAnimation(_ carousel: iCarousel) {
+        lblPeriodoEscolhido.text = periodos[carousel.currentItemIndex]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView( _ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            return periodos.count
-    }
-    
-    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//            return contas[row].nome
-        return periodos[row]
-    }
-    
-    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//            txtConta.text = contas[row].nome
-//            selectedConta = contas[row]
     }
 
 }
